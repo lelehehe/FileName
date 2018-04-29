79466757fee8362bf120834a7a2763a90cb75c81
mike zhang <mzhang@zulily.com>
2018-04-28 22:30:53 -0700

    works with mkv srt case

--- FileRename.py
+++ FileRename.py
@@ -3,23 +3,35 @@ import re
 
 SE_REGEX = re.compile(r"[sS]\d{2}[eE]\d{2}")
 
-videoPath = "/Volumes/Plex/Download/review/Criminal Minds/S01/test"
+videoPath = "/Volumes/Plex/Download/review/Criminal Minds/S01"
 path = Path(videoPath)
-
-def purge(fileName):
-    newName = SE_REGEX.split(fileName)[0]
-    print(' name=', newName)
-    s01e01 = SE_REGEX.search(fileName).group().upper()
-    print(', match=',  s01e01)
-    return newName + s01e01
 
 
 files = [p for p in Path(videoPath).iterdir() if p.is_file()]
 
+# private functions
+def purge(fileName):
+    found = SE_REGEX.search(fileName)
+    if found is None: return None
+    s01e01 = found.group().upper()
+    print(', match=',  s01e01)
+    newName = SE_REGEX.split(fileName)[0]
+    print(' name=', newName)
+    return newName + s01e01
+
 for file in files: 
     # old name: file.stem
-    #print(file.name, file.stem)
+    print(file.name)
     stem = purge(file.stem)
-    print(' stem=', stem)
-    file.rename(Path(file.parent, stem + file.suffix))
+    if stem is not None: 
+        print('rename ', file.stem, ' to ', stem)
+        if (file.suffix == '.ass' or file.suffix == '.srt'):
+            suffix = ('.chs'+file.suffix)
+        else:
+            suffix = file.suffix
+        print(' suffix=', suffix)
+        file.rename(Path(file.parent, stem + suffix))
+
+
+
 
